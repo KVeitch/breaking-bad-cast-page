7b146413bd3ba1fbdde1afb1069d6f6b8721b168
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-07-08 11:27:20 -0600

    Updates

---
+++ precache-manifest.273bee990de0ebf214dd81fd2e908a01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa27a48f93103e70a661b6b006224285",
+    "url": "/breaking-bad-cast-page/index.html"
+  },
+  {
+    "revision": "d635f326ff89a2a08720",
+    "url": "/breaking-bad-cast-page/static/css/main.5f0ff7d5.chunk.css"
+  },
+  {
+    "revision": "77387c0b31199519c547",
+    "url": "/breaking-bad-cast-page/static/js/2.9cbbf616.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-cast-page/static/js/2.9cbbf616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d635f326ff89a2a08720",
+    "url": "/breaking-bad-cast-page/static/js/main.8fad3211.chunk.js"
+  },
+  {
+    "revision": "49af839ad0c2686c4df9",
+    "url": "/breaking-bad-cast-page/static/js/runtime-main.0534deaf.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-cast-page/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-cast-page/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-cast-page/static/media/spinner.235b9b3e.gif"
+  }
+]);
