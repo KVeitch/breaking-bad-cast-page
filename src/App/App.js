8df07d20ac55e2700766541d807662c7de70fdd0
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-07-08 10:55:34 -0600

    Modify axios get link for searching

--- src/App/App.js
+++ src/App/App.js
@@ -8,10 +8,11 @@ import axios from 'axios'
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(()=>{
     const fetchItems = async () =>{
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
 
       console.log(result.data)
       setItems(result.data)
